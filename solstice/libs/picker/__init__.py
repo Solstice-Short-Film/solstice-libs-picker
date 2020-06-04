435cd003bc65e040de2bafa1aafe11018985b492
Tomas Poveda <tpovedatd@gmail.com>
2020-06-04 17:11:39 +0200

    Added base repository structure and files

---
+++ solstice/libs/picker/__init__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Initialization module for solstice-libs-picker
+"""
+
+
+def init():
+    """
+    Initializes module
+    """
+
+    print('Init Solstice picker dependencies ...')
+
